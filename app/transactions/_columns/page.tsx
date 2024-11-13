6d848b7e0cb4315a10a9f8d2622fd4ab5b1f697d
amandadeogr <amandadeog@gmail.com>
2024-11-13 18:35:45 -0300

    feat: update transactions table to include Edit and Delete actions, enhance rendering of category, payment method, date, and amount

--- app/transactions/_columns/page.tsx
+++ app/transactions/_columns/page.tsx
@@ -1,9 +1,11 @@
 "use client";
-import renderTransactionCategory from "@/helper/renderTransactionCategory";
-import renderTransactionPaymentMethod from "@/helper/renderTransactionPaymentMethod";
+import { renderTransactionCategory } from "@/helper/renderTransactionCategory";
+import { renderTransactionPaymentMethod } from "@/helper/renderTransactionPaymentMethod";
 import renderBadge from "@/helper/renderTransactionTypeBadge";
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
+import EditTransaction from "../_components/EditTransaction";
+import DeleteTransaction from "../_components/DeleteTransaction";
 
 export const transactionsColumns: ColumnDef<Transaction>[] = [
   {
@@ -18,26 +20,51 @@ export const transactionsColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
-    cell: ({ row: { original: transaction } }) =>
-      renderTransactionCategory(transaction),
+    cell: ({ row: { original: transaction } }) => {
+      return renderTransactionCategory(transaction.category);
+    },
   },
   {
     accessorKey: "paymentMethod",
     header: "Método",
     cell: ({ row: { original: transaction } }) => {
-      renderTransactionPaymentMethod(transaction);
+      return renderTransactionPaymentMethod(transaction.paymentMethod);
     },
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) => {
+      return transaction.date !== null
+        ? new Date(transaction.date).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          })
+        : null;
+    },
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) => {
+      return new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount));
+    },
   },
   {
     accessorKey: "actions",
-    header: "",
+    header: "Ações",
+    cell: () => {
+      return (
+        <div>
+          <EditTransaction />
+
+          <DeleteTransaction />
+        </div>
+      );
+    },
   },
 ];
