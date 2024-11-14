a167c3dc0b21e63893a5e32edcd201efed0c36c0
amandadeogr <amandadeog@gmail.com>
2024-11-14 20:15:43 -0300

    feat: implement DeleteTransaction component with toast notification on successful deletion

--- app/transactions/_components/DeleteTransaction.tsx
+++ app/transactions/_components/DeleteTransaction.tsx
@@ -1,30 +1,78 @@
+"use client";
+import { deleteTransaction } from "@/app/_actions/delete-transaction";
 import { Button } from "@/app/_components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/app/_components/ui/dialog";
-import { TrashIcon } from "lucide-react";
+import { useToast } from "@/app/_hooks/use-toast";
+import { Transaction } from "@prisma/client";
+import { TrashIcon, XCircleIcon } from "lucide-react";
+import { useState } from "react";
 
-const DeleteTransaction = () => {
+interface DeleteTransactionProps {
+  transaction: Transaction;
+}
+
+const DeleteTransaction = ({ transaction }: DeleteTransactionProps) => {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const onDelete = async () => {
+    try {
+      await deleteTransaction({ id: transaction.id });
+      setDialogIsOpen(false);
+      toast({
+        title: "Transação deletada com sucesso!",
+        variant: "destructive",
+        className: "text-white",
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const { toast } = useToast();
   return (
-    <Dialog>
+    <Dialog
+      open={dialogIsOpen}
+      onOpenChange={(open) => {
+        setDialogIsOpen(open);
+      }}
+    >
       <DialogTrigger asChild>
         <Button variant={"ghost"}>
           <TrashIcon size={20} />
         </Button>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="w-96">
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+          <DialogTitle className="flex items-center gap-3">
+            <XCircleIcon size={18} className="fill-danger text-black" />
+            Deseja deletar essa transação?
+          </DialogTitle>
+          <DialogDescription className="py-3">
+            Uma vez deletada não poderá recuperá-la.
           </DialogDescription>
         </DialogHeader>
+        <DialogFooter className="justify-end">
+          <DialogClose asChild>
+            <Button variant={"outline"}>Cancelar</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button
+              variant={"destructive"}
+              type="submit"
+              onClick={onDelete}
+              className="text-white"
+            >
+              Deletar
+            </Button>
+          </DialogClose>
+        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
